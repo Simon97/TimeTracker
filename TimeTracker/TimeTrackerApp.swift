acb07115e02caf1522c1e297b149454442363cc8
Simon Svendsgaard Nielsen <simon97nielsen@gmail.com>
2024-01-23 08:20:17 +0100

    Fixed previews

--- TimeTracker/TimeTrackerApp.swift
+++ TimeTracker/TimeTrackerApp.swift
@@ -11,10 +11,22 @@ import SwiftUI
 @main
 struct TimeTrackerApp: App {
     
+    let modelContainer: ModelContainer
+    
+    // For some reason initializing this first makes the previews work (the TaskView preview
+    // did not work previously)
+    init() {
+        do {
+            modelContainer = try ModelContainer(for: Project.self)
+        } catch {
+            fatalError("Could not initialize ModelContainer")
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(for: Project.self)
+        .modelContainer(modelContainer)
     }
 }
